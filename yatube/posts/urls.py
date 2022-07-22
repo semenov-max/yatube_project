43d7413638f8499e0450c2a7221d3a1f80b7c25d
Max Semenov <jwsemenov@gmail.com>
2022-07-22 02:35:50 -0400

    бета-версия на проверку

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,9 +1,10 @@
 # posts/urls.py
 from django.urls import path
-
 from . import views
+
+app_name = 'posts'
 
 urlpatterns = [
     path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('group_list/<slug:slug>', views.group_posts, name = 'group_list'),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 # Create your views here.
 
 def index(request):
     """YaTube - main."""
-    return HttpResponse('Main page of the YaTube service.')
+    TEMPLATE = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts' : posts
+    }
+    return render(request, TEMPLATE, context)
 
 def group_posts(request, slug):
-    """Posts filtered by groups."""
-    return HttpResponse(f'My posts {slug}')
+    """YaTube - page group_posts."""
+    TEMPLATE = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, TEMPLATE, context)
